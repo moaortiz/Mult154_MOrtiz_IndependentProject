d0f4ca7466d30a84bcc503e534edc511c5a16668
Monika Ortiz <monika.ortiz.aof@gmail.com>
2020-09-22 10:06:15 -0700

    First Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rbPlayer;
+    private Vector3 direction = Vector3.zero;
+    public float speed = 10.0f;
+    public GameObject spawnPoint = null;
+    private Vector3 jumpDirection;
+    public float jumpSpeed;
+    public GameObject camerOffset;
+    //public float multiplier;
+
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        float horMove = Input.GetAxis("Horizontal");
+        float verMove = Input.GetAxis("Vertical");
+
+        direction = new Vector3(horMove, 0, verMove);
+        //jumpDirection = new Vector3(horMove, jumpSpeed, verMove);
+    }
+
+    void FixedUpdate()
+    {
+        rbPlayer.AddForce(direction * speed, ForceMode.Impulse);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rbPlayer.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            Debug.Log("Space bar pressed");
+        }
+    }
+
+    private void Respawn()
+    {
+        rbPlayer.MovePosition(spawnPoint.transform.position);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Hazard"))
+        {
+            Respawn();
+        }
+    }
+
+   private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Page"))
+        {
+            Destroy(other.gameObject);
+            //Debug.Log("Enters collider");
+        }
+    }
+}
---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float rotSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseXInput = Input.GetAxis("Mouse X");
+        transform.Rotate(Vector3.right, mouseXInput * Time.deltaTime * rotSpeed);
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //transform.Rotate(Vector3.up, horizontalInput * Time.deltaTime * rotSpeed);
+    }
+}
